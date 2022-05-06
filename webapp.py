fdd0c34447b0a58405c816bf8b41af8ac5bf8e1c
ramirezanthony <ramirezanthony347@gmail.com>
2022-05-06 09:05:35 -0700

    5/6

--- webapp.py
+++ webapp.py
@@ -76,13 +76,13 @@ def authorized():
     return render_template('message.html', message=message)
 
 
-@app.route('/page1')
+@app.route('/discussion')
 def renderPage1():
     if 'user_data' in session:
         user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
     else:
         user_data_pprint = '';
-    return render_template('page1.html',dump_user_data=user_data_pprint)
+    return render_template('discussion.html',dump_user_data=user_data_pprint)
 
 @app.route('/page2')
 def renderPage2():
