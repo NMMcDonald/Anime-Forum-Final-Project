bb47d950e742255d205044ef557a894a2e4a6849
P1 <Marco80512>
2022-05-06 09:51:18 -0700

    css

--- webapp.py
+++ webapp.py
@@ -87,11 +87,7 @@ def authorized():
 
 @app.route('/discussion')
 def renderdiscussion():
-    if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
-    else:
-        user_data_pprint = '';
-    return render_template('discussion.html',dump_user_data=user_data_pprint)
+    return render_template('discussion.html')
 
 @app.route('/page2')
 def renderPage2():
